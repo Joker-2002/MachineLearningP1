dc9907237873e6c7ca94224d9984aa79f625dbc6
Debasish Kumar <debasishk636@gmail.com>
2024-06-04 19:37:27 +0530

    app building done and predit pipeline

--- src/utils.py
+++ src/utils.py
@@ -1,8 +1,10 @@
 import os
 import sys
+import dill
+import pickle
+
 import pandas as pd
 import numpy as np
-import dill
 
 from src.exception import CustomException
 from sklearn.metrics import r2_score
@@ -52,3 +54,12 @@ def evaluate_models(X_train,y_train,X_test,y_test,models,param):
     
     except Exception as e:
         raise CustomException(e,sys)
+    
+
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return pickle.load(file_obj)
+
+    except Exception as e:
+        raise CustomException(e, sys)
