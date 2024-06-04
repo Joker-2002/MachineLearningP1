24b4a8251e714fb4c0a5fa8b53a08911ec410d52
Debasish Kumar <debasishk636@gmail.com>
2024-06-04 13:11:48 +0530

    Model trainer done

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -7,7 +7,10 @@ from dataclasses import dataclass
 
 from src.exception import CustomException
 from src.logger import logging
-from src.components.data_transformation import DataTransformation,DataTransformationConfig
+from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformationConfig
+from src.components.model_training import ModelTrainerConfig
+from src.components.model_training import ModelTrainer
 
 @dataclass
 class DataIngestionConfig:
@@ -51,3 +54,6 @@ if __name__ == "__main__":
 
     data_transformation=DataTransformation()
     train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data,test_data)
+
+    modeltrainer = ModelTrainer()
+    print(modeltrainer.initiate_model_trainer(train_arr,test_arr))
